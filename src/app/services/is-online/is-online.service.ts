944fce5aa658fbd0e6fe34112d03950cbb09c4ba
timokluser-dev <timo@timokluser.ch>
2021-12-09 21:58:52 +0100

    update IsOnlineService

--- src/app/services/is-online/is-online.service.ts
+++ src/app/services/is-online/is-online.service.ts
@@ -18,7 +18,10 @@ export class IsOnlineService {
     });
   }
 
-  public register(): void {
+  public async register(): Promise<void> {
+    const isOnline = await this.isOnline;
+    !isOnline && this.showToastOfflineAppStarted();
+
     this.listener = Network.addListener('networkStatusChange', status => {
       console.debug('Network status changed', status);
       !status.connected && this.showToastOffline().then();
@@ -30,9 +33,17 @@ export class IsOnlineService {
   }
 
   private async showToastOffline(): Promise<void> {
+    this.showToast("Lost network connection. We will save you data when you're back online.");
+  }
+
+  private async showToastOfflineAppStarted(): Promise<void> {
+    this.showToast("You are currently offline. We will get your data when you're back online.");
+  }
+
+  private async showToast(msg: string) {
     const TOAST_DURATION_MS = 10 * 1000;
     this.toast = await this.toastController.create({
-      message: "Lost network connection. We will save you data when you're back online",
+      message: msg,
       duration: TOAST_DURATION_MS,
       buttons: ['Dismiss'],
     });
